b6f5582cb42b193ec0d9f84de57ddb37c86be7b9
Shivang Seth <ronakseth2003@gmail.com>
2022-12-17 00:26:12 +0530

    Add p1.py to my project

---
+++ p1.py
@@ -0,0 +1,25 @@
+def addition(a,b):
+    return a+b
+def substraction(a,b):
+    return a-b
+def multiplication(a,b):
+    return a*b
+def division(a,b):
+    return a/b
+x=int(input("Enter the first number :"))
+y=int (input("Enter the second number :"))
+choice = int(input("1)Addition   2)substracion  3)multiplication  4)Division"))
+if choice == 1:
+    a = addition(x,y)
+    print(a)
+elif choice == 2:
+    s = substraction(x,y)
+    print(s)
+elif choice == 3:
+    m = multiplication(x,y)
+    print(m)
+elif choice == 4:
+    d = division(x,y)
+    print(d)
+else:
+    print("NOT POSSIBLE")
