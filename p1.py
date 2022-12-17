4139ecf09f763eb7350d14ed294e0968ee3aa85f
Shivang Seth <ronakseth2003@gmail.com>
2022-12-17 14:09:31 +0530

    Added extra spaces for clarity and reduced time complexity

--- p1.py
+++ p1.py
@@ -1,25 +1,30 @@
 def addition(a,b):
-    return a+b
+    return a+b 
+
 def substraction(a,b):
     return a-b
+
 def multiplication(a,b):
     return a*b
+
 def division(a,b):
     return a/b
+
 x=int(input("Enter the first number :"))
-y=int (input("Enter the second number :"))
+
 choice = int(input("1)Addition   2)substracion  3)multiplication  4)Division"))
-if choice == 1:
-    a = addition(x,y)
-    print(a)
-elif choice == 2:
+
+y=int (input("Enter the second number :"))
+
+s=0            
+if choice == '+':
+    s = addition(x,y)
+elif choice == '-':
     s = substraction(x,y)
-    print(s)
-elif choice == 3:
-    m = multiplication(x,y)
-    print(m)
-elif choice == 4:
-    d = division(x,y)
-    print(d)
+elif choice == '*':
+    s = multiplication(x,y)
+elif choice == '/':
+    s = division(x,y)
 else:
     print("NOT POSSIBLE")
+print(s)
